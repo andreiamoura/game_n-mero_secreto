9b48a88bef34d9511e3ad10ebe5fb6901cc80f87
Andreia Borges <andreiadasilvaborges12@gmail.com>
2024-02-08 21:49:53 -0300

    concatenando o numeroSecreto ao valor

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = 4;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
 //se chute for igua numero secreto ---> você descobriu o número secreto(5)
 if (chute == numeroSecreto) {
+    alert(`Você descobriu o Número secreto ${numeroSecreto}`);
     //console.log('você descobriu o número secreto(5)');
 
     // se nao for igual numero secreto
