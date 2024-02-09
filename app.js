c741a326242249afcb56a5f962d7a6d6f8a533ab
Andreia Borges <andreiadasilvaborges12@gmail.com>
2024-02-08 21:37:20 -0300

    condicional usando if e else

---
+++ app.js
@@ -0,0 +1,23 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10');
+
+//se chute for igua numero secreto ---> você descobriu o número secreto(5)
+if (chute == numeroSecreto) {
+    //console.log('você descobriu o número secreto(5)');
+
+    // se nao for igual numero secreto
+} else {
+    alert('Você errou');
+    //console.log('vc errou');
+}
+
+
+
+
+
+
+
+
+
